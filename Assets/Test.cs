61f7538e816ea10b1ca470e06a3362eff9c8b381
Howard <hhhhhoward@gmail.com>
2017-04-19 16:26:48 +0700

    FirstVersion FirstVersionfor test

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //进入触发器执行的代码
+        Debug.Log("trigger");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+        //进入碰撞器执行的代码
+    }
+}
